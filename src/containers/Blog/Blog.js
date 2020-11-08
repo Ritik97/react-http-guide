e14cc0cdb494065edbe4fb4f485dd4c1b11db007
Ritik Sinha <ritiksinha91@gmail.com>
2020-11-08 20:40:43 +0530

    Transformed fetched data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -15,16 +15,23 @@ class Blog extends Component {
 
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(response => {
-            this.setState({posts: response.data});
-            console.log(response.data);
-        });
+            .then(response => {
+                const posts = response.data.slice(0, 4); //fetching data from index 0 to 4
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Ritik'
+                    }
+                });
+                this.setState({ posts: updatedPosts });
+                console.log(response);
+            });
     };
 
-    render () {
+    render() {
 
         const posts = this.state.posts.map(post => {
-            return <Post key={ post.id } title={ post.title } />
+            return <Post key={post.id} title={post.title} author={post.author} />
         })
 
         return (
