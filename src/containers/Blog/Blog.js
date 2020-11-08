1e7885a6a6e5aa8da245db8aea4d46998b063984
Ritik Sinha <ritiksinha91@gmail.com>
2020-11-08 20:30:58 +0530

    Rendering received data into the app

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,20 +9,28 @@ import './Blog.css';
 /** In Blog component, we will fetch the list of Posts from jsonplaceholder and render it. */
 class Blog extends Component {
 
+    state = {
+        posts: []
+    };
+
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
         .then(response => {
-            console.log(response);
+            this.setState({posts: response.data});
+            console.log(response.data);
         });
     };
 
     render () {
+
+        const posts = this.state.posts.map(post => {
+            return <Post key={ post.id } title={ post.title } />
+        })
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
